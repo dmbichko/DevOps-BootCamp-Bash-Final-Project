a1b24c52cdf5611706b6c505f7ddbc8dac9412a6
Ubuntu <ubuntu@ip-172-31-95-214.ec2.internal>
2022-07-12 16:46:04 +0000

    1st

--- transfer.sh
+++ transfer.sh
@@ -1,10 +1,11 @@
-#!/bin/curl
+#!/bin/bash
 
-currentVersion="0.0.1"
+readonly CURRENT_VERSION="1.23.0"
 
 httpSingleUpload()
 {
-    response=$(curl -A curl --upload-file "$1" "https://transfer.sh/$2") || { echo "Failure!"; return 1;}
+  response=$(curl --progress-bar --upload-file "$file" "https://transfer.sh/$file_name") || { echo "Failure!"; return 1;} 
+ #response=$(curl -A curl --upload-file "$1" "https://transfer.sh/$2") || { echo "Failure!"; return 1;}
 }
 
 printUploadResponse()
@@ -17,12 +18,31 @@ EOF
 
 singleUpload()
 {
-  filePath=$(echo "$1" | sed s:"~":"$HOME":g)
-  if ! -f "$filePath" ;then { echo "Error: invalid file path"; return 1;}; fi
-  tempFileName=$(echo "$1" | sed "s/.*\///")
-  echo "Uploading $tempFileName"
-  httpSingleUpload "$filePath $tempFileName"
+  file="$ARG"
+  file_name=$(basename "$file")
+  echo "Uploading " $file_name
+  httpSingleUpload "$file $file_name"
 }
-
-singleUpload "$1" || exit 1
-printUploadResponse
+singleDowload()
+{
+  echo "funcDomw"
+}
+if [ "$1" == "-h" ]; then
+  echo "Description: Bash tool to transfer files from the command line. 
+    Usage: 
+    -d  ...
+    -h  Show the help ... 
+    -v  Get the tool version 
+    Examples: 
+    <Write a couple of examples, how to use your tool>"
+elif [ "$1" == "-v" ]; then
+  echo "$CURRENT_VERSION"
+elif [[ "$#" -eq 1 ]]; then
+  singleUpload "$1" || exit 1
+  printUploadResponse
+elif [[ "$#" -ne 1 ]]; then
+  for ARG in "$@"; do
+    singleUpload "$ARG" || exit 1
+    printUploadResponse
+  done
+fi
