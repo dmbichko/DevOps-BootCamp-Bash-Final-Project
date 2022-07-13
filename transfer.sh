84217aedf453157eea84e4da59838c7aceaf4f84
Ubuntu <ubuntu@ip-172-31-95-214.ec2.internal>
2022-07-13 07:00:35 +0000

    shell

--- transfer.sh
+++ transfer.sh
@@ -10,7 +10,7 @@ httpSingleUpload()
 
 printUploadResponse()
 {
-#fileID=$(echo "$response" | cut -d "/" -f 4)
+fileID=$(echo "$response" | cut -d "/" -f 4)
   printf '%s' "Transfer File URL: ""$response""\n"
 }
 
@@ -25,7 +25,7 @@ singleDowload()
 {
   echo "Downloading " "$file_download"
 
-  (curl --progress-bar https://transfer.sh/"$id_download"/"$file_download" -o "$dir_download"/"$file_download") || { echo "Failure!"; return 1;}
+  (curl --progress-bar https://transfer.sh/"$fileID"/"$file_download" -o "$dir_download"/"$file_download") || { echo "Failure!"; return 1;}
 }
 printDownloadResponse()
 {
@@ -39,7 +39,7 @@ if [ $# -eq 0 ];then printf "No arguments specified.\n"
   printf "Usage: transfer <file|directory> | transfer <file_name> \n"; 
   exit 1;
 fi
-if [[ "${arg_array[0]}" == "-h" && ${#arg_array[*]} -eq 1 ]]; then
+if [[ "${arg_array[0]}" == "-h" && ${#arg_array[@]} -eq 1 ]]; then
   echo "Description: Bash tool to transfer files from the command line. 
     Usage: 
     -d  ...
@@ -47,9 +47,9 @@ if [[ "${arg_array[0]}" == "-h" && ${#arg_array[*]} -eq 1 ]]; then
     -v  Get the tool version 
     Examples: 
     <Write a couple of examples, how to use your tool>"
-elif [[ "${arg_array[0]}" == "-v" && ${#arg_array[*]} -eq 1 ]]; then  
+elif [[ "${arg_array[0]}" == "-v" && ${#arg_array[@]} -eq 1 ]]; then  
   echo "$CURRENT_VERSION"
-elif [[ "${arg_array[0]}" == "-d" && ${#arg_array[*]} -eq 4 ]]; then
+elif [[ "${arg_array[0]}" == "-d" && ${#arg_array[@]} -eq 4 ]]; then
   id_download=${arg_array[2]}
   dir_download=${arg_array[1]}
   file_download=${arg_array[3]}
